2c90aaa41d3c23538f40d41aa8675b32eb2a7f58
unknown <you@example.com>
2020-09-11 07:42:57 +0530

    Updates

---
+++ precache-manifest.42e1588e3a5f5aaccbcb9dbdc0d58a45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35195e16cf14721de70a29493f68a2c1",
+    "url": "/WetherApp/index.html"
+  },
+  {
+    "revision": "d21cb16f30bf5261661c",
+    "url": "/WetherApp/static/css/main.c862a70b.chunk.css"
+  },
+  {
+    "revision": "f1adcfc2b197854bd140",
+    "url": "/WetherApp/static/js/2.f3bccc6e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/WetherApp/static/js/2.f3bccc6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d21cb16f30bf5261661c",
+    "url": "/WetherApp/static/js/main.ae3da07d.chunk.js"
+  },
+  {
+    "revision": "843730adfc5cd87eca59",
+    "url": "/WetherApp/static/js/runtime-main.e60b7d2d.js"
+  }
+]);
